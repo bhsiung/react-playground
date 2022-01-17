87d092b1a757e92ed9ed63e03381fe89cc36016b
Biing-yann Hsiung (bear) <bhsiung@linkedin.com>
2022-01-17 01:45:06 -0800

    add router v6

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 import SlidingPuzzle from './SlidingPuzzle'
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <div className="bd">
-      <SlidingPuzzle />
-    </div>
+    <Router>
+      <nav>
+        <Link to="/">home</Link>
+        <Link to="/sliding-puzzle">sliding puzzle</Link>
+      </nav>
+      <main className="bd">
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/sliding-puzzle" element={<SlidingPuzzle />} />
+        </Routes>
+      </main>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
