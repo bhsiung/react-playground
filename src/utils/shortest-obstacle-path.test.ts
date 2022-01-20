6254d165996b88e20f2e1a4e42b64ef7aa6cfe94
Biing-yann Hsiung (bear) <bhsiung@linkedin.com>
2022-01-19 16:19:06 -0800

    wip2

---
+++ src/utils/shortest-obstacle-path.test.ts
@@ -0,0 +1,11 @@
+import {getPath } from './shortest-obstacle-path'
+
+describe('test getPath', () => {
+  it('works', () => {
+    const pathMap 
+    expect(breakPattern('abc')).toEqual([
+      { value: 'a', hasStar: false },
+      { value: 'b', hasStar: false },
+      { value: 'c', hasStar: false },
+    ])
+
--- src/utils/shortest-obstacle-path.ts
+++ src/utils/shortest-obstacle-path.ts
@@ -1,42 +1,98 @@
 interface Connection {
-    isObstacle: boolean
-    pos: Pos
+  isObstacle: boolean
+  pos: Pos
 }
 type Pos = [number, number]
 
 export function shortestPath(
-    grid: number[][],
-    k: number,
-    visited = new Set<string>(),
-    queue:Pos[] = []
+  grid: number[][],
+  k: number,
+  visited = new Set<string>(),
+  queue: Pos[] = [],
+  pathMap: Record<string, string> = {}
 ): number {
-    const shortestPath = grid.length + grid[0].length - 1 
-    let bestPath = Number.MAX_SAFE_INTEGER
-    if (queue.length === 0) queue.push(grid[0][0])
-    while(queue.length > 0) {
-        const tile = queue.shift()
-        const connections = findConnections()
-        
-        for (let connection of connections) {
-            const posString = connection.pos.join(',')
-            if (!visited.has(posString)) {
-                if (k > 0 && connection.isObstacle) {
-                    const pathIfEliminate = shortestPath(
-                        grid,
-                        k - 1, 
-                        cloneVisited(visited, connection), 
-                        cloneQueue(queue, connection)
-                    )
-                    bestPath = Math.min(pathIfEliminate, bestPath)
-                    if (bestPath <= shortestPath) return bestPath
-                }
-            }
-        }
+  const shortestPossiblePath = grid.length + grid[0].length - 2
+  let bestPath = Number.MAX_SAFE_INTEGER
+  if (queue.length === 0) queue.push([0, 0])
+  while (queue.length > 0) {
+    const tile = queue.shift() as Pos
+    const connections = findConnections(grid, tile)
+    // finish early if connection include destination
+    for (let connection of connections) {
+      if (
+        connection.pos[0] === grid.length - 1 &&
+        connection.pos[1] === grid[0].length - 1
+      ) {
+        return Math.min(getPath(pathMap, tile.join(',')), bestPath)
+      }
     }
-    
-    // get path
-    return Math.min(path, bestPath)
-};
-function findConnections(grid: number[][], pos: Pos): Connection[] {
-    
+
+    for (let connection of connections) {
+      const posString = connection.pos.join(',')
+      if (!visited.has(posString)) {
+        if (connection.isObstacle) {
+          if (k > 0) {
+            const pathIfEliminate = shortestPath(
+              grid,
+              k - 1,
+              cloneVisited(visited, connection.pos),
+              [...queue, connection.pos],
+              { ...pathMap, posString: tile.join(',') }
+            )
+            bestPath = Math.min(pathIfEliminate, bestPath)
+            if (bestPath <= shortestPossiblePath) return bestPath
+          }
+        } else {
+          visited.add(posString)
+          queue.push(connection.pos)
+          pathMap[posString] = tile.join(',')
+        }
+      }
+    }
+  }
+
+  // unable to get path
+  return Math.min(-1, bestPath)
+}
+
+function findConnections(grid: number[][], [i, j]: Pos): Connection[] {
+  const vectors: Pos[] = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+  ]
+  const connections: Connection[] = []
+  for (let [offsetI, offsetJ] of vectors) {
+    if (
+      i + offsetI >= 0 &&
+      i + offsetI <= grid.length - 1 &&
+      j + offsetJ >= 0 &&
+      j + offsetJ <= grid[0].length - 1
+    ) {
+      const newPos: Pos = [i + offsetI, j + offsetJ]
+      connections.push({
+        pos: newPos,
+        isObstacle: grid[newPos[0]][newPos[1]] === 1,
+      })
+    }
+  }
+
+  return connections
+}
+function cloneVisited(visited: Set<string>, posToAdd: Pos): Set<string> {
+  const newSet = new Set<string>()
+  visited.forEach((item) => newSet.add(item))
+  newSet.add(posToAdd.join(','))
+  return newSet
+}
+
+export function getPath(pathMap: Record<string, string>, endPoint: Pos): number {
+  let posStr = endPoint.join(',')
+  let count = 0
+  while (posStr !== '0,0') {
+    posStr = pathMap[posStr]
+    count++
+  }
+  return count + 1
 }
